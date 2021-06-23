764fae6047838a5fd235de31c69b047e017f464a
credish <celeste.redish@gmail.com>
2021-06-23 19:07:40 -0400

    add in buttons

--- blackjack_game/script.js
+++ blackjack_game/script.js
@@ -1,25 +1,62 @@
 let firstCard=10;
-let secondCard=11;
+let secondCard=4;
+let cards = [firstCard, secondCard]; // ordered list of cards
 let sum=(firstCard + secondCard);
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let startGame = document.getElementById("start-game");
+let newCard = document.getElementById("new-card");
+let messageEl = document.getElementById("message-el");
+let cardDisplay = document.getElementById("cards-display");
+let sumDisplay = document.getElementById("sum-display");
+// let sumDisplay = document.querySelector("#sum-display");
 
-if (sum <= 20){
-	message = "Do you want to draw a new card?";
+function startTheGame() {
+    renderGame();
+}
+
+function renderGame(){
+    cardDisplay.textContent = "Cards: " + cards[0] + " " + cards[1] + " ";
+    sumDisplay.textContent = "Sum: " + sum;
     
-}
-else if (sum === 21) {
-    message = "Whoohoo! You've got Blackjack!";
-    hasBlackJack = true;
-}
-else {
-	message = "You're out of the game!";
-    isAlive=false;
+    if (sum <= 20){
+        message = "Do you want to draw a new card?";
+        
+    }
+    else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    }
+    else {
+        message = "You're out of the game!";
+        isAlive=false;
+    }
+
+    messageEl.textContent = message;
+
+    console.log(message);
+    console.log(sum);
 }
 
-// Cash Out!
-console.log(hasBlackJack);
-console.log(isAlive);
+function getNewCard() {
+    message = "Drawing a new card from the deck!";
+    messageEl.textContent = message;
+    let card = 2;
+    sum += card;
+    renderGame();
+    // cardDisplay.textContent = "Cards: " + cards[];
+}
+
+startGame.addEventListener("click", startTheGame);
+newCard.addEventListener("click", getNewCard);
+
+
+
+
+
+
+
+
 
 
