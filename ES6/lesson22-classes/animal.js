9234d443a66b658838590c57ceb7d17bff900e03
credish <celeste.redish@gmail.com>
2021-11-22 17:06:27 -0500

    update es6

--- ES6/lesson22-classes/animal.js
+++ ES6/lesson22-classes/animal.js
@@ -8,8 +8,28 @@ export class Animal {
         console.log(sound);
     }
 
+    get metaData() {
+        return `Type: ${this.type}, Legs: ${this.legs}`;
+    }
+
     // static allows us create a function in class where we don't have to create an instance - for using with a large library
     static return10() {
         return 10;
     }
 };
+
+// export class Cat extends Animal {
+//     constructor(type, legs, tail) {
+        // calls functions from parent class
+//         super(type, legs);
+//         this.tail=tail;
+//     }
+// }
+
+// Can override method - make noise
+export class Cat extends Animal {
+    makeNoise(sound = "meow") { 
+        console.log(sound);
+    }
+}
+
--- ES6/lesson22-classes/index.js
+++ ES6/lesson22-classes/index.js
@@ -1,6 +1,6 @@
-import { Animal } from './animal.js';
+// import { Animal } from './animal.js';
 
-let cat = new Animal('Cat', 4);
+// let cat = new Animal('Cat', 4);
 
 // cat.legs = 3;
 // cat.makeNoise();
@@ -10,4 +10,12 @@ let cat = new Animal('Cat', 4);
 // console.log(cat.type);
 // console.log(cat.legs);
 
-console.log(Animal.return10());
+// console.log(Animal.return10());
+
+import { Animal, Cat } from './animal.js';
+
+let cat = new Cat('Cat', 4);
+
+cat.makeNoise();
+
+console.log(cat.metaData);
